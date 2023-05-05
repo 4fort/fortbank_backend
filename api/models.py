5ed135ab3dde847a5c2ea23a38ed5fe71f9c59a9
Dexter Fort <dexterkun16@gmail.com>
2023-05-05 23:00:19 +0800

    init

--- api/models.py
+++ api/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class User(models.Model):
     owner_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200)
-    card_num = models.IntegerField()
+    card_num = models.IntegerField(unique=True)
     card_pin = models.IntegerField()
-    balance = models.FloatField()
+    balance = models.FloatField(default=0)
 
     def __str__(self):
         return self.owner_name + ' (' + str(self.card_num) + ') P' + str(self.balance)
